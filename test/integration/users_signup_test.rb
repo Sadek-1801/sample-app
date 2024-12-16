7cc30e291ba8bcb525ff2679d0ab8b244e0c7741
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-12-16 14:39:59 +0600

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example user",
+                                         email: "user@example.com",
+                                         password: "foobar",
+                                         password_confirmation: "foobar"
+      }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
