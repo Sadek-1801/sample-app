d5bed4b0d68b15a45ad2226a50501e72e785927f
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-12-21 14:36:22 +0600

    Implement advanced login

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,6 +1,6 @@
 function addToggleListerner(selectedId, menuId, toggleClass){
   let selectedItem = document.querySelector(`#${selectedId}`);
-  selectedItem.addEventListener("click", function(e){
+  selectedItem?.addEventListener("click", function(e){
     e.preventDefault();
     let menu = document.querySelector(`#${menuId}`);
     menu.classList.toggle(`${toggleClass}`)
