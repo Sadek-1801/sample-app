de3f32e64d3f2373a9d77d48e10134222c9024b8
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-12-22 11:56:45 +0600

    Add update user without authorization

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = users(:one)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "",
+                                              email: "fo@invali",
+                                              password: "foo",
+                                              password_confirmation: "bar" }}
+    assert_template 'users/edit'
+    assert_select "div.alert", "The form contains 4 errors"
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "sample user"
+    email = "sample@user.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" }}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+end
