4e4eb35f2f6469a32e62d96ccc5e0187e8cc880b
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-12-24 02:22:25 +0600

    Finish user RESTful APIs actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,7 +32,7 @@ module SessionsHelper
     end
   end
 
-  # Returns true if the given use is the current user.
+  # Returns true if the given user is the current user.
   def current_user?(user)
     user&. == current_user
   end
